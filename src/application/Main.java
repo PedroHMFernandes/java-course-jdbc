ae1e0efedf84ac76a06e116752e7ab4ec9692900
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-09-23 22:13:25 -0300

    Recuperar dados

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.createStatement();
+
+			rs = st.executeQuery("select * from department");
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection(conn);
+		}
 	}
+
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -41,6 +43,36 @@ public class DB {
 			return props;
 		} catch (IOException e) {
 			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeConnection(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
